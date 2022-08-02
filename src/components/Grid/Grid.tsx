d1e31ff9e2fd693c1cf284aeb0e68065110b6c58
nedakinduryte <neda.kinduryte@gmail.com>
2022-08-02 20:42:16 +0100

    improved grid logic

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,21 +1,54 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { cloneDeep } from 'lodash';
 
+import { StateType } from '../../entities';
 import Cell from '../Cell';
-import { addMines, createGrid } from '../../utils';
+import { createGrid, openCells } from '../../utils';
 
 import './Grid.css';
 
-const Grid: FC = () => {
-  const gridSize = 16;
-  const totalMines = 40;
+interface GridType {
+  gridSize?: number;
+  mineCount?: number;
+}
 
-  const grid = createGrid(gridSize);
-  const gridWithMines = addMines(totalMines, grid);
+const Grid: FC<GridType> = ({ gridSize = 16, mineCount = 40 }) => {
+  const [grid, updateGrid] = useState<StateType[][]>(createGrid(gridSize, mineCount));
+  const [gameOver, setGameOver] = useState(false);
+
+  const onClick = (e: any) => {
+    const x = Number(e.target.attributes[1].value);
+    const y = Number(e.target.attributes[2].value);
+
+    const gridCopy = cloneDeep(grid);
+
+    if (!gameOver) {
+      if (gridCopy[x][y].mineCount === 0 && !gridCopy[x][y].isMine) {
+        const newGrid = openCells(gridCopy, x, y);
+        updateGrid(newGrid);
+        return;
+      }
+
+      if (gridCopy[x][y].mineCount > 0 && !gridCopy[x][y].isMine) {
+        gridCopy[x][y].isOpen = true;
+        updateGrid(gridCopy);
+        return;
+      }
+
+      if (gridCopy[x][y].isMine) {
+        console.log('gameOver');
+        setGameOver(true);
+        return;
+      }
+    }
+  };
 
   return (
-    <div className="grid-wrapper">
-      {gridWithMines.map((row, xIdx) =>
-        row.map((cell, yIdx) => <Cell key={`${xIdx}-${yIdx}`} {...cell} />)
+    <div className="grid-wrapper" onClick={onClick}>
+      {grid.map((row, xIdx) =>
+        row.map((cell, yIdx) => (
+          <Cell key={`${xIdx}-${yIdx}`} position={{ x: xIdx, y: yIdx }} {...cell} />
+        ))
       )}
     </div>
   );
