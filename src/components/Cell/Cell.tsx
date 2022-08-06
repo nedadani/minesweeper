91d9efaa5e3684fb30c18e75cbe22a56e888a9e0
nedakinduryte <neda.kinduryte@gmail.com>
2022-08-06 09:52:34 +0100

    updated logic in Cell

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,10 +1,24 @@
-import React, { FC } from 'react';
+import React, { FC, SyntheticEvent, useState } from 'react';
 
-import { CellTypes } from '../../entities';
+import { StateType } from '../../entities';
 import './Cell.css';
 
-const Cell: FC<CellTypes> = () => {
-  return <span className="cell-wrapper"></span>;
+interface CellType extends StateType {
+  position: { x: number; y: number };
+}
+
+const Cell: FC<CellType> = ({ position, mineCount, isFlagged, isMine, isOpen }) => {
+  if (isOpen)
+    return (
+      <span className="cell" data-x-position={position.x} data-y-position={position.y}>
+        {isMine ? 'x' : mineCount}
+      </span>
+    );
+  return (
+    <span className="cell" data-x-position={position.x} data-y-position={position.y}>
+      {isFlagged ? '@' : ''}
+    </span>
+  );
 };
 
 export default Cell;
