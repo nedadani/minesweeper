90d059051c46d9305351137cd61a369aa55b43d3
nedakinduryte <neda.kinduryte@gmail.com>
2022-08-06 10:11:04 +0100

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+
+import Grid from './components/Grid';
+
 import './App.css';
 
 function App() {
-  return <main className="App"></main>;
+  return (
+    <main className="App">
+      <Grid />
+    </main>
+  );
 }
 
 export default App;
