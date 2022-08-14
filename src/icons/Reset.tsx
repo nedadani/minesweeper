2bf2dcaca18b936bc3203ab624aae5b63df12413
nedakinduryte <neda.kinduryte@gmail.com>
2022-08-14 14:57:00 +0100

    added a reset icon

---
+++ src/icons/Reset.tsx
@@ -0,0 +1,19 @@
+const Reset = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="12"
+    height="12"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-rotate-cw restart"
+  >
+    <polyline points="23 4 23 10 17 10"></polyline>
+    <path d="M20.49 15a9 9 0 1 1-2.12-9.36L23 10"></path>
+  </svg>
+);
+
+export { Reset };
