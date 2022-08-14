cb1edde216b230d706ec7052ba8e953e8afa971c
nedakinduryte <neda.kinduryte@gmail.com>
2022-08-14 15:32:21 +0100

    added Toolkit to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
 import { SMALL, MEDIUM } from './constants';
+import { GridOptions } from './entities';
 import Header from './components/Header';
+import Toolkit from './components/Toolkit';
 import Grid from './components/Grid';
 
 import styles from './App.module.css';
 
 const App = () => {
-  const [options, setOptions] = useState({ gridSize: 16, totalMines: 40 });
+  const [options, setOptions] = useState<GridOptions>({ gridSize: 16, totalMines: 40 });
   const [flagCount, setFlagCount] = useState(0);
   const [gameOver, setGameOver] = useState(false);
 
-  useEffect(() => setGridSize, []);
+  useEffect(() => setGridSize(), []);
 
   useEffect(() => {
     window.addEventListener('resize', setGridSize);
@@ -19,13 +21,14 @@ const App = () => {
   });
 
   const setGridSize = () => {
-    window.innerWidth < 680 ? setOptions(SMALL) : setOptions(MEDIUM);
+    window.innerWidth < 680 ? setOptions({ ...SMALL }) : setOptions({ ...MEDIUM });
   };
 
   return (
     <main className={styles.app}>
       <section>
         <Header isGameOver={gameOver} flagCount={flagCount} mineCount={options.totalMines} />
+        <Toolkit />
         <Grid
           {...options}
           isGameOver={gameOver}
